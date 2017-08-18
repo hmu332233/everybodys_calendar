7a016730dcbc3bfc92e814f7cd8fce4a22a29668
Ban Sang Min <tkdals1119@gmail.com>
2017-08-18 10:50:06 +0000

    migration 5.1제거

--- db/migrate/20170816103915_create_impressions_table.rb
+++ db/migrate/20170816103915_create_impressions_table.rb
@@ -1,4 +1,4 @@
-class CreateImpressionsTable < ActiveRecord::Migration[5.1]
+class CreateImpressionsTable < ActiveRecord::Migration
   def self.up
     create_table :impressions, :force => true do |t|
       t.string :impressionable_type
