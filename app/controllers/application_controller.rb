af09a67a3da8ecfa0cc35eeba1986e57ee8e9729
minung Han <hmu332233@likelion.org>
2017-08-19 01:36:13 +0900

    [추가] 로그 디자인 추가, posts 추가

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_action :set_basic_data
+  
+  def set_basic_data
+    @calendars = Calendar.all
+    @posts = Post.all
+  end
 end
