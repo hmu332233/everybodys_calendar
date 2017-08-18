746cfea3ad56c62ad16682eaf07a327132e9077e
minung Han <hmu332233@likelion.org>
2017-08-19 02:05:56 +0900

    [수정] seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,19 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Calendar.create(name: '전북대')
-Event.create(title: '첫번째 일정', start:'2017-08-08', end: '', calendar_id: 1, url: '/events/125')
-Event.create(title: '두번째 일정', start:'2017-08-15', end: '', calendar_id: 1, url: '/events/126')
-Event.create(title: '세번째 일정', start:'2017-08-20', end: '', calendar_id: 1, url: '/events/127')
-Event.create(title: '네번째 일정', start:'2017-08-21', end: '2017-08-25', url: '/events/128')
+Calendar.create(name: '멋쟁이사자처럼')
+event1 = Event.create(title: '첫번째 일정', start:'2017-08-08', end: '', calendar_id: 1, url: '/events/125')
+event2 = Event.create(title: '두번째 일정', start:'2017-08-15', end: '', calendar_id: 1, url: '/events/126')
+event3 = Event.create(title: '세번째 일정', start:'2017-08-20', end: '', calendar_id: 1, url: '/events/127')
+event4 = Event.create(title: '네번째 일정', start:'2017-08-21', end: '2017-08-25', url: '/events/128')
+
+h1 = HashTag.create(name: "멋사")
+h2 = HashTag.create(name: "해커톤")
+
+
+event1.hash_tags << h1
+event1.hash_tags << h1
+
+event2.hash_tags << h1
+
+event3.hash_tags << h2
