f6e4c70cecdd313b2fa68ef497c91373df974ae5
minung Han <hmu332233@likelion.org>
2017-08-18 23:07:39 +0900

    [수정] 캘린더 메인 디자인

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Event.create(title: '첫번째 일정', start:'2017-08-08', end: '', url: '/events/125')
-Event.create(title: '두번째 일정', start:'2017-08-15', end: '', url: '/events/126')
-Event.create(title: '세번째 일정', start:'2017-08-20', end: '', url: '/events/127')
+Calendar.create(name: '전북대')
+Event.create(title: '첫번째 일정', start:'2017-08-08', end: '', calendar_id: 1, url: '/events/125')
+Event.create(title: '두번째 일정', start:'2017-08-15', end: '', calendar_id: 1, url: '/events/126')
+Event.create(title: '세번째 일정', start:'2017-08-20', end: '', calendar_id: 1, url: '/events/127')
 Event.create(title: '네번째 일정', start:'2017-08-21', end: '2017-08-25', url: '/events/128')
