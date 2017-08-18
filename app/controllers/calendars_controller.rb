187839043464b6755e7402c295574ce796680a00
minung Han <hmu332233@likelion.org>
2017-08-18 20:45:18 +0900

    [수정] calendar index 비율 조정

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -4,9 +4,13 @@ class CalendarsController < ApplicationController
 
   def show
     @calendar_id = params[:id]
+    
+    @calendar = Calendar.find(@calendar_id)
   end
 
   def create
+    name = params[:name]
+    Calendar.create(name: name)
   end
 
   def update
